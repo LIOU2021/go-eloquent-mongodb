bc2d280954c8b0bf3bed61ba265e75ba83942423
LIOU <aa78789898tw@gmail.com>
2022-12-05 02:00:32 +0800

    opt model

--- tests/models/user.go
+++ tests/models/user.go
@@ -8,13 +8,6 @@ type User struct {
 	UpdatedAt uint64  `bson:"updated_at"`
 }
 
-type UserCreateData struct {
-	Name      string `bson:"name"`
-	Age       uint16 `bson:"age"`
-	CreatedAt uint64 `bson:"created_at"`
-	UpdatedAt uint64 `bson:"updated_at"`
-}
-
 type UserUpdateData struct {
 	Name      string `bson:"name"`
 	Age       uint16 `bson:"age"`
